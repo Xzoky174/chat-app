9b0364bec2d025b674c294e5cbb83d5a70722ff0
Xzoky174 <xzoky123@gmail.com>
2022-10-03 18:22:44 +0400

    Update UI for signup page

--- client/src/routes/signup/+page.svelte
+++ client/src/routes/signup/+page.svelte
@@ -12,6 +12,8 @@
 
 	let error: string;
 
+	let signing_up = false;
+
 	let userStore: Writable<User | null> = getContext('user');
 
 	onMount(async () => {
@@ -22,6 +24,7 @@
 
 	async function formSubmit() {
 		if (!passValid() || !usernameValid()) return;
+		signing_up = true;
 
 		const res = await fetch('http://localhost:3000/auth/signup', {
 			credentials: 'include',
@@ -42,10 +45,15 @@
 			const user = await authenticated();
 			userStore.set(user);
 		}
+
+		signing_up = false;
 	}
 
 	const passValid = () => {
-		if (password.length <= 8) {
+		if (password === '') {
+			error = '';
+			return false;
+		} else if (password.length <= 8) {
 			error = 'Password must be more than 7 characters';
 			return false;
 		} else if (password !== confirmPassword) {
@@ -57,28 +65,94 @@
 		}
 	};
 	const usernameValid = () => {
-		const isValid = username.length >= 4;
-		error = isValid ? '' : 'Username must be more than 7 characters';
-
-		return isValid;
+		if (username === '') {
+			error = '';
+			return false;
+		} else if (username.length < 4) {
+			error = 'Username must be more than 3 characters';
+			return false;
+		} else if (username.length > 20) {
+			error = 'Username must not be more than 20 characters';
+			return false;
+		} else {
+			error = '';
+			return true;
+		}
 	};
 </script>
 
-<form on:submit|preventDefault={formSubmit}>
-	<input type="text" name="username" id="username" bind:value={username} on:input={usernameValid} />
+<div class="main">
+	<form on:submit|preventDefault={formSubmit}>
+		<input
+			placeholder="Username"
+			type="text"
+			name="username"
+			id="username"
+			bind:value={username}
+			on:input={usernameValid}
+		/>
 
-	<input type="password" name="password" id="password" bind:value={password} on:input={passValid} />
-	<input
-		type="password"
-		name="confirm-password"
-		id="confirm-password"
-		bind:value={confirmPassword}
-		on:input={passValid}
-	/>
+		<input
+			placeholder="Password"
+			type="password"
+			name="password"
+			id="password"
+			bind:value={password}
+			on:input={passValid}
+		/>
+		<input
+			placeholder="Confirm Password"
+			type="password"
+			name="confirm-password"
+			id="confirm-password"
+			bind:value={confirmPassword}
+			on:input={passValid}
+		/>
 
-	<input type="submit" value="Sign Up" />
-</form>
+		<input type="submit" value={signing_up ? 'Loading' : 'Sign Up'} disabled={signing_up} />
 
-{#if error}
-	<p>{error}</p>
-{/if}
+		{#if error}
+			<p>{error}</p>
+		{/if}
+	</form>
+</div>
+
+<style>
+	.main {
+		height: calc(100vh - var(--navbar-height));
+		display: grid;
+		place-items: center;
+	}
+	form {
+		text-align: center;
+		max-width: 260px;
+	}
+	input[type='text'],
+	input[type='password'] {
+		margin-bottom: 5px;
+		display: block;
+		font-size: 22px;
+		padding: 8px 12px;
+		border: 2px solid #ced4da;
+		border-radius: 4px;
+	}
+	input[type='submit'] {
+		margin-top: 12px;
+		color: #fff;
+		cursor: pointer;
+		border: 0;
+		border-radius: 4px;
+		transition: 0.25s;
+		text-decoration: none;
+		background-color: #3498db;
+		font-size: 18px;
+		padding: 8px 16px;
+	}
+	input[type='submit'][disabled],
+	input[type='submit']:disabled {
+		opacity: 0.45 !important;
+	}
+	input[type='submit']:hover {
+		opacity: 0.85;
+	}
+</style>
